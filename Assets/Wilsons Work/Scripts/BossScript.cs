e28d531674a65fabdedf24bf25ac26805b129784
wsprouse <118293505+wsprouse@users.noreply.github.com>
2023-03-10 10:26:45 -0800

    animation struggling, Fireball projectile
    I'm having hard time with animation but made the bull shoot 6 fireballs

--- Assets/Wilsons Work/Scripts/BossScript.cs
+++ Assets/Wilsons Work/Scripts/BossScript.cs
@@ -6,10 +6,15 @@ public class BossScript : MonoBehaviour
 {
     public bool sideCollision;
     public Animator animator;
+    public Transform[] Brokenbullstatue;
+    public float speed;
+    public GameObject projectile;
+    public Transform[] holes;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        StartCoroutine ("PossessedBullStatue");
     }
 
     // Update is called once per frame
@@ -19,6 +24,31 @@ public class BossScript : MonoBehaviour
         {
             animator.SetTrigger("IsHurt");
             sideCollision = false;
+        }
+    }
+
+    IEnumerator PossessedBullStatue()
+    {
+        /*
+        while (transform.position.x != Brokenbullstatue[0].position.x)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(Brokenbullstatue[0].position.x, transform.position.y), speed);
+
+            yield return null;
+        }
+        */
+
+        //transform.localScale=new Vector2(-1, 1);
+
+        yield return new WaitForSeconds(1f);
+
+        int i = 0;
+        while (i<6)
+        {
+           GameObject Fireball= (GameObject)Instantiate(projectile,holes[Random.Range(0,2)].position, Quaternion.identity);
+            Fireball.GetComponent<Rigidbody2D>().velocity = Vector2.left * 5;
+            i++;
+            yield return new WaitForSeconds(.7f);
         }
     }
 }
